92fd380486cbfa7a0da5ce671e8b1a6e4520655b
bornedj <bornedj@miamioh.edu>
2021-06-30 18:23:21 -0400

    believe i have aworking check for duplicates

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+
+  const [name, setName] = useState("name");
+  const [phone, setPhone] = useState("phone")
+  const [email, setEmail] = useState("email");
+  const [duplicate, setDuplicate] = useState(false);
+
+  useEffect(() => {
+    if (!props.contacts) {
+      setDuplicate(() => false)
+    } else {
+      setDuplicate(() => {
+        const check = props.contacts.some(contact = contact.name === name)//if a single one of the contacts matches we will return true for duplicate
+        return check;
+      })
+    } 
+  }, [name])//check if name gets changed
 
   const handleSubmit = (e) => {
     e.preventDefault();
